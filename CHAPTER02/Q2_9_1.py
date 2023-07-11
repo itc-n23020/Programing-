922ecc41dafd33c2401e1050c4ad7fdbc7381217
haruru nakazato <n23020@std.it-college.ac.jp>
2023-07-11 05:38:54 +0000

    add Q2_9

---
+++ CHAPTER02/Q2_9_1.py
@@ -0,0 +1,4 @@
+"apple" in ["pineapple", "orange", "banana"]
+
+# 解答
+# False
---
+++ CHAPTER02/Q2_9_2.py
@@ -0,0 +1,12 @@
+x = True
+y = False
+z = None
+
+1 x and z is None
+2 not x or not y
+3 x and not y and z is None
+
+# 解答
+# 1 True
+# 2 True
+# 3 True
