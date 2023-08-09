4ec87c442b3739d70f98d17e4330b68a51709145
haruru nakazato <n23020@std.it-college.ac.jp>
2023-08-09 05:47:59 +0000

    add Q5_1

---
+++ CHAPTER05/Q5_1_1.py
@@ -0,0 +1,12 @@
+# プログラミングを実行して「Suzuki」という結果を取得して、もとのリストから要素を消すために、下記のプログラミングの空欄に記述すべきコードはどれでしょう？
+
+members = ['Yamada', 'Tanaka', 'Satou', 'Suzuki']
+□
+print(name)
+
+① name = members.remove(3)
+② name = members.clear(3)
+③ name = members.destroy(3)
+④ name = members.pop(3)
+
+# 正解:④
