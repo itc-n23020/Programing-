fbd6c79b600f796aae843fc88c4b129779147deb
haruru nakazato <n23020@std.it-college.ac.jp>
2023-08-17 05:44:49 +0000

    add Q7_1

---
+++ CHAPTER07/Q7_1_3.py
@@ -0,0 +1,10 @@
+# 次のプログラムを実行したときに発生する例外の方はどれでしょうか？
+
+int('3.5')
+
+① IndexError
+② KeyError
+③ ZeroDivisionError
+④ ValueError
+
+# 正解:④
---
+++ CHAPTER07/Q7_1_4.py
@@ -0,0 +1,11 @@
+# 次のプログラムを実行したときに発生する例外の方はどれでしょうか？
+
+my_list = []
+my_list.insert(3)
+
+① IndexError
+② KeyError
+③ ZeroDivisionError
+④ ValueError
+
+# 正解③
